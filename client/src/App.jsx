0c65578f61e4112e55b8a36f0553d75508667f87
ankit <ankitrsomani02@gmail.com>
2023-07-31 22:38:04 +0530

    changed urls

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import io from 'socket.io-client';
 import './index.css';
 import Chat from './Chat';
 
-const socket = io.connect('http://localhost:3001');
+const socket = io.connect('https://vibe-chat-server.onrender.com/');
 
 const App = () => {
 
